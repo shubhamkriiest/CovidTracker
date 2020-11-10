a184408a5837e452bc9300ac617359f725587169
shubham kumar <shubhamiit8298@gmail.com>
2020-11-10 22:36:08 +0530

    covidtracker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CoronaService} from './services/corona.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'covidTracker';
+
+  countries:any
+  country:any
+  confirmed:number
+  recovered:number
+  deaths:number
+
+  constructor(private corona:CoronaService){}
+
+  ngOnInit(){
+    this.corona.getCountries().subscribe((data)=>{
+      console.log(data)
+      this.countries = data
+
+    })
+  }
+  getCoronaData(){
+    this.corona.getCoronaRealtimeData(this.country).subscribe((data)=>{
+      console.log(data)
+      var index = data.length - 1
+      this.confirmed = data[index].Confirmed
+      this.recovered = data[index].Recovered
+      this.deaths = data[index].Deaths
+    })
+
+  }
+  getCountry(country:any){
+    this.country = country;
+  }
 }
